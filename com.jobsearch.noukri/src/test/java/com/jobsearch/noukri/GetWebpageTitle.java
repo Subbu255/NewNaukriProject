71539d044bfbee3a5a75a04be2dc286a35c390fd
Subbu255 <subramani25597@gmail.com>
2023-08-18 18:05:58 +0530

    ...

---
+++ com.jobsearch.noukri/src/test/java/com/jobsearch/noukri/GetWebpageTitle.java
@@ -0,0 +1,38 @@
+package com.jobsearch.noukri;
+
+import org.bouncycastle.asn1.BEROctetStringGenerator;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GetWebpageTitle {
+	
+	WebDriver driver;
+	
+	
+	
+	@Test
+	public void getWebpageTitle(String browser, String url) {
+		
+		if(browser.contains("chrome")) {
+			
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();	
+		}
+		
+		else if (browser.contains("firefox")) {
+			
+			WebDriverManager.firefoxdriver().setup();
+			driver= new FirefoxDriver();
+			
+			
+		}
+		
+		driver.get(url);
+		
+	}
+
+}
